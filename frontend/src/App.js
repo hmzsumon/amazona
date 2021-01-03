51ce8eb5394c963a0fdf40f0c75c42156ea85897
hmzsumon <zsumonn@gmail.com>
2021-01-03 16:13:50 +0600

    dainamicaly disply products in home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import data from './data';
+
 function App() {
 	return (
 		<div className='grid-container'>
@@ -14,179 +17,40 @@ function App() {
 			</header>
 			<main>
 				<div className='row center'>
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
+					{data.products.map((product) => (
+						<div key={product._id} className='card'>
+							<a href={`/product/${product._id}`}>
+								<img
+									className='medium'
+									src={product.image}
+									alt={product.name}
+								/>
 							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-half-o'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-o'></i>
-								</span>
+							<div className='card-body'>
+								<a href={`/product/${product.name}`}>
+									<h2>{product.name}</h2>
+								</a>
+								<div className='rating'>
+									<span>
+										<i className='fa fa-star'></i>
+									</span>
+									<span>
+										<i className='fa fa-star'></i>
+									</span>
+									<span>
+										<i className='fa fa-star'></i>
+									</span>
+									<span>
+										<i className='fa fa-star-half-o'></i>
+									</span>
+									<span>
+										<i className='fa fa-star-o'></i>
+									</span>
+								</div>
+								<div className='price'>${product.price}</div>
 							</div>
-							<div className='price'>$120</div>
 						</div>
-					</div>
-
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
-							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-half-o'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-o'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-o'></i>
-								</span>
-							</div>
-							<div className='price'>$120</div>
-						</div>
-					</div>
-
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
-							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-o'></i>
-								</span>
-							</div>
-							<div className='price'>$120</div>
-						</div>
-					</div>
-
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
-							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-half-o'></i>
-								</span>
-								<span>
-									<i className='fa fa-star-o'></i>
-								</span>
-							</div>
-							<div className='price'>$120</div>
-						</div>
-					</div>
-
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
-							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-							</div>
-							<div className='price'>$120</div>
-						</div>
-					</div>
-
-					<div className='card'>
-						<a href='product.html'>
-							<img className='medium' src='./images/p1.jpg' alt='product' />
-						</a>
-						<div className='card-body'>
-							<a href='product.html'>
-								<h2>Nike Slime Shirts</h2>
-							</a>
-							<div className='rating'>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-								<span>
-									<i className='fa fa-star'></i>
-								</span>
-							</div>
-							<div className='price'>$120</div>
-						</div>
-					</div>
+					))}
 				</div>
 			</main>
 			<footer className='row center'>
