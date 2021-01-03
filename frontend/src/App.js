43180ad098bb4c9c835976f83226e1cb8bc21f40
hmzsumon <zsumonn@gmail.com>
2021-01-03 15:38:12 +0600

    create react app and transfar template file

---
+++ frontend/src/App.js
@@ -0,0 +1,199 @@
+function App() {
+	return (
+		<div className='grid-container'>
+			<header className='row'>
+				<div>
+					<a className='brand' href='index.htl'>
+						amazona
+					</a>
+				</div>
+				<div>
+					<a href='index.html'>Cart</a>
+					<a href='signin.html'>Sign In</a>
+				</div>
+			</header>
+			<main>
+				<div className='row center'>
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-half-o'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-o'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-half-o'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-o'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-o'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-o'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-half-o'></i>
+								</span>
+								<span>
+									<i className='fa fa-star-o'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+
+					<div className='card'>
+						<a href='product.html'>
+							<img className='medium' src='./images/p1.jpg' alt='product' />
+						</a>
+						<div className='card-body'>
+							<a href='product.html'>
+								<h2>Nike Slime Shirts</h2>
+							</a>
+							<div className='rating'>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+								<span>
+									<i className='fa fa-star'></i>
+								</span>
+							</div>
+							<div className='price'>$120</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer className='row center'>
+				Copy Right &copy; 2021 zakaria All right reserbd
+			</footer>
+		</div>
+	);
+}
+
+export default App;
